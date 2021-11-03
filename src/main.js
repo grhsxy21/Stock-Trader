a90a431c64e6639d1bc1422408e2165559872dcd
lqqqqqqqy <1003098534@qq.com>
2021-11-03 22:08:29 +0800

    lqy 11/3

--- src/main.js
+++ src/main.js
@@ -1,17 +1,19 @@
 import Vue from 'vue';
-import VueResource from 'vue-resource';
 import vuetify from './plugins/vuetify';
 import App from './App.vue';
 import VueRouter from 'vue-router';
 import { routes } from './routes';
 import { store } from './store/store';
+import axios from 'axios'
+import VueAxios from 'vue-axios';
 
 const router = new VueRouter({
 	routes: routes
 });
 
 Vue.use(VueRouter);
-Vue.use(VueResource);
+Vue.use(VueAxios, axios);
+// Vue.prototype.$http = axios
 
 Vue.filter("dollarFormat", function(x) {
 	return x.toLocaleString(undefined,{ style: 'currency', currency: 'USD' });
--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@
 import Home from './components/Home.vue';
 import Portfolio from './components/Portfolio.vue';
 import Stocks from './components/Stocks.vue';
+import Login from './components/Login.vue';
 
 export const routes = [
-   { path: '', component: Home},
+   { path: '/', component: Login},
    { path: '/stocks', component: Stocks},
-   { path: '/portfolio', component: Portfolio}
+   { path: '/portfolio', component: Portfolio},
+   { path:'/home', component: Home}
 ]
