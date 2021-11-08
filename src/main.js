4c1abd033414dc31635a3f52f3e2f5e006ca2523
GRHSXY <1399784328@qq.com>
2021-11-08 19:57:47 +0800

    注册登录

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import VueResource from 'vue-resource';
+//import VueResource from 'vue-resource';
 import vuetify from './plugins/vuetify';
 import App from './App.vue';
 import VueRouter from 'vue-router';
@@ -8,15 +8,18 @@ import { store } from './store/store';
 //*添加ElementUI */
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-
+import axios from 'axios'
+import VueAxios from 'vue-axios';
 
 const router = new VueRouter({
 	routes: routes
 });
 
 Vue.use(VueRouter);
-Vue.use(VueResource);
+//Vue.use(VueResource);
 Vue.use(ElementUI);
+Vue.use(VueAxios, axios);
+// Vue.prototype.$http = axios
 
 Vue.filter("dollarFormat", function(x) {
 	return x.toLocaleString(undefined,{ style: 'currency', currency: 'USD' });
